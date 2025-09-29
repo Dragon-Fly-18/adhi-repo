15d79515cdefddfffa411705584e47309f541139
Dragon-Fly-18 <adhikeshavan050505@gmail.com>
2025-09-29 21:59:44 +0530

    init

---
+++ shadcn-ui/src/App.tsx
@@ -0,0 +1,27 @@
+import { Toaster } from '@/components/Ui/sonner';
+import { TooltipProvider } from '@/components/Ui/tooltip';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Index from './pages/Index';
+import NotFound from './pages/NotFound';
+import { LanguageProvider } from './context/LanguageContext';
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <LanguageProvider>
+      <TooltipProvider>
+        <Toaster />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </LanguageProvider>
+  </QueryClientProvider>
+);
+
+export default App;
